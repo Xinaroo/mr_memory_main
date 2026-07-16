3d5a280f3359ba75ea6d85a57ea3d51a60c253d4
Xinaroo <123162050+Xinaroo@users.noreply.github.com>
2026-07-16 17:25:04 +0900

    Update GOOGLE_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // Apps Script를 웹 앱으로 배포한 뒤 /exec 주소를 아래에 붙여넣으세요.
-  GOOGLE_SCRIPT_URL: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxPAWV9gtZ5rz-kOb9YfWjukLyIhGOqSVLUU1KtQCGSxb9iODuUh120OOaDFuo1nJgr0w/exec",
 
   // 참가자 화면에서 연구자용 Block/Layout 메뉴를 숨기려면 false로 바꾸세요.
   SHOW_RESEARCHER_PANEL: true,
